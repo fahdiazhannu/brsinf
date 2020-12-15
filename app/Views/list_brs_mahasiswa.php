f468650d32d4577119da5ab4c5ef7f230e0813e6
pedroandika6200 <fedro.a62@gmail.com>
2020-12-15 17:57:47 +0700

    halaman dosen

---
+++ app/Views/list_brs_mahasiswa.php
@@ -0,0 +1,38 @@
+<?php
+use App\Models\MatkulModel;
+$model = new MatkulModel;
+?>
+
+<div class="container mt-5">
+        <h2> List BRS yang diambil Mahasiswa</h2>
+        <div class="row mt-3">
+            <div class="col-sm-12">
+            <?php if (session()->getFlashdata('msg')) : ?>
+                <div class="alert alert-success"><?= session()->getFlashdata('msg') ?></div>
+            <?php endif ?>
+                    <table class="table table-striped" id="tableUser">
+                        <thead>
+                            <tr>
+                                <th>Nama Mahasiswa</th>
+                                <th>NIM</th>
+                                <th>Kode Matakuliah yang diambil</th>
+                                <th></th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php if ($mahasiswa) : ?>
+                                <?php foreach ($mahasiswa as $mhs) : ?>
+                                    <form action="<?= base_url('admin/dosen/lihat'); ?>" method="POST">
+                                    <tr>
+                                        <td><?= $mhs['nama']; ?></td>
+                                        <td><?= $mhs['nmr_induk']; ?></td>
+                                        <td><?= $mhs['kode_mk']; ?></td>
+                                        <td><a href="<?= base_url('admin/dosen/lihat/'.$mhs['id']); ?>" type="submit" id="send_form" class="btn btn-success">Lihat</a></td>
+                                    </tr>
+                                    </form>
+                                <?php endforeach; ?>
+                            <?php endif; ?>
+                        </tbody>
+                    </table>
+            </div>
+        </div>
